1c054d6aad9dd198614eb8f8269f7474768af7bc
David Cristóbal <david@juanked.es>
2020-06-12 13:27:45 +0200

    Añadido nombre y formateado

--- DMUTEX.2019/Proceso/main.c
+++ DMUTEX.2019/Proceso/main.c
@@ -1,10 +1,14 @@
 /* DMUTEX (2009) Sistemas Operativos Distribuidos
- * Código de Apoyo
+ * Cï¿½digo de Apoyo
  *
- * ESTE CÓDIGO DEBE COMPLETARLO EL ALUMNO:
+ * ESTE Cï¿½DIGO DEBE COMPLETARLO EL ALUMNO:
  *    - Para desarrollar las funciones de mensajes, reloj y
- *      gestión del bucle de tareas se recomienda la implementación
+ *      gestiï¿½n del bucle de tareas se recomienda la implementaciï¿½n
  *      de las mismas en diferentes ficheros.
+ */
+
+/* Nombre: David CristÃ³bal Pascual
+ * MatrÃ­cula: 160336 
  */
 
 #include <stdio.h>
@@ -14,37 +18,38 @@
 
 int puerto_udp;
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
   int port;
-  char line[80],proc[80];
+  char line[80], proc[80];
 
-  if(argc<2)
+  if (argc < 2)
   {
-    fprintf(stderr,"Uso: proceso <ID>\n");
+    fprintf(stderr, "Uso: proceso <ID>\n");
     return 1;
   }
 
-  /* Establece el modo buffer de entrada/salida a línea */
-  setvbuf(stdout,(char*)malloc(sizeof(char)*80),_IOLBF,80);
-  setvbuf(stdin,(char*)malloc(sizeof(char)*80),_IOLBF,80);
+  /* Establece el modo buffer de entrada/salida a lï¿½nea */
+  setvbuf(stdout, (char *)malloc(sizeof(char) * 80), _IOLBF, 80);
+  setvbuf(stdin, (char *)malloc(sizeof(char) * 80), _IOLBF, 80);
 
-  puerto_udp=1111; /* Se determina el puerto UDP que corresponda.
-                      Dicho puerto debe estar libre y quedará
+  puerto_udp = 1111; /* Se determina el puerto UDP que corresponda.
+                      Dicho puerto debe estar libre y quedarï¿½
                       reservado por dicho proceso. */
 
-  fprintf(stdout,"%s: %d\n",argv[1],puerto_udp);
+  fprintf(stdout, "%s: %d\n", argv[1], puerto_udp);
 
-  for(;fgets(line,80,stdin);)
+  for (; fgets(line, 80, stdin);)
   {
-    if(!strcmp(line,"START\n"))
+    if (!strcmp(line, "START\n"))
       break;
 
-    sscanf(line,"%[^:]: %d",proc,&port);
+    sscanf(line, "%[^:]: %d", proc, &port);
     /* Habra que guardarlo en algun sitio */
 
-    if(!strcmp(proc,argv[1]))
-    { /* Este proceso soy yo */ }
+    if (!strcmp(proc, argv[1]))
+    { /* Este proceso soy yo */
+    }
   }
 
   /* Inicializar Reloj */
