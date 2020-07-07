8e08ab09261d3a92d44b8d73ef6ad3ed6ce19103
David Cristóbal <david@juanked.es>
2020-07-07 20:06:22 +0200

    Creadas las clases y sus métodos, close #12

---
+++ DMUTEX.2019/Proceso/clock.c
@@ -0,0 +1,19 @@
+#include "clock.h"
+
+void printClock(int procesos, int *estados, char *proceso)
+{
+    printf("%s: clock(", proceso);
+    int i = 0;
+    while (i < procesos - 1)
+    {
+        printf("%d, ", estados[i]);
+        i++;
+    }
+    printf("%d)\n", estados[procesos]);
+}
+
+void addToClock(int procesos, int *estados, char *proceso)
+{
+    estados[procesos]++;
+    printf("%s: TICK\n", proceso);
+}
